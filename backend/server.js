e183beb66bfa377ecb0ac8b0916d81ee04b0d163
samarth06trivedi <samarth2004trivedi@gmail.com>
2025-02-03 23:26:12 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI ; 
 const app = express();
-const db = monk(MONGO_URI);
+const db = monk(process.env.MONGO_URI);
 const tatianaCollection = db.get("Tatiana-Herwitz");
 
 app.use(cors());
@@ -23,6 +22,5 @@ app.get("/api/tatiana", async (req, res) => {
   }
 });
 
-// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
